85690523e7564a1414a5a296f77beeb4bc9dc2ae
aeh1707 <abdelhadi.hireche@outlook.com>
2022-05-10 10:07:06 +0100

    Fix mobile menu

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
 const burger_button = document.querySelector(".menu-button");
 const responsive_menu = document.querySelector(".responsive");
 const close_icon = document.querySelector(".close-icon");
-const links = document.querySelector(".child1");
-const links2 = document.querySelector(".child2");
-const links3 = document.querySelector(".child3");
+const links = document.querySelectorAll(".main-nav-link2");
+
+for (let i=0; i<links.length; i++) {
+  links[i].addEventListener('click', function (e) {
+    responsive_menu.style.display = "none";
+  }) 
+}
 
 burger_button.addEventListener("click", function () {
   responsive_menu.style.display = "flex"
 });
 
 close_icon.addEventListener("click", function () {
-  responsive_menu.style.display = "none"
-});
-
-links.addEventListener("click", function () {
-  responsive_menu.style.display = "none"
-});
-
-links2.addEventListener("click", function () {
-  responsive_menu.style.display = "none"
-});
-
-links3.addEventListener("click", function () {
   responsive_menu.style.display = "none"
 });
