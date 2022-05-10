00dd186e419ea86872c16553f4e433b3104def63
aeh1707 <abdelhadi.hireche@outlook.com>
2022-05-10 16:47:59 +0100

    Create dynamic content for .fourth-box

--- script.js
+++ script.js
@@ -33,9 +33,42 @@ mobilePopX.addEventListener('click', () => {
 // POP Up
  const project1 = {
    name: "Multi Post Stories",
-   description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+   description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lo",
    image: "img/mobile-popup.png",
    alt: "Number 10 with red background",
    technologies: ["html", "Css", "Bootsrap"],
    links: ["#", "#"]
  }
+
+ const project2 = {
+  name: "Keeping track of hundreds  of components website",
+  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+  image: "img/lady-athlet.png",
+  alt: "Lady athlet project",
+  technologies: ["html", "Css", "Bootsrap", "Ruby"],
+  links: ["#", "#"]
+}
+
+const cardProject = document.querySelector('.fourth-box');
+const cardProjectHeading = document.createElement('h3');
+cardProjectHeading.textContent = project1.name;
+const descriptionNode = document.createElement('p');
+descriptionNode.textContent = project1.description;
+const techList = document.createElement('ul');
+const firstTech = document.createElement('li');
+firstTech.className = 'programming-boxes';
+firstTech.textContent = project1.technologies[0];
+const secondTech = document.createElement('li');
+secondTech.textContent = project1.technologies[1];
+const thirdTech = document.createElement('li');
+thirdTech.textContent = project1.technologies[1];
+const fourthTech = document.createElement('li');
+fourthTech.textContent = project1.technologies[3];
+
+cardProject.appendChild(cardProjectHeading);
+cardProject.appendChild(descriptionNode);
+techList.appendChild(firstTech);
+techList.appendChild(secondTech);
+techList.appendChild(thirdTech);
+techList.appendChild(fourthTech);
+cardProject.appendChild(techList);
